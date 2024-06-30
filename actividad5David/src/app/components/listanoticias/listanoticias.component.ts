5b5c7a92b028d8fdfa34eb8e66107b5986c35517
david <david-moreno-@hotmail.es>
2024-06-30 18:29:27 +0200

    components listanoticias

---
+++ actividad5David/src/app/components/listanoticias/listanoticias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listanoticias',
+  standalone: true,
+  imports: [],
+  templateUrl: './listanoticias.component.html',
+  styleUrl: './listanoticias.component.css'
+})
+export class ListanoticiasComponent {
+
+}
